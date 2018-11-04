25fb06b4c02001bc54b45a198e23b29192b7ea68
Esko Suomi <esko.suomi@siili.com>
2018-11-04 19:31:43 +0200

    remove libs/messenger
    We don't need this.

--- libs/messenger/project.clj
+++
@@ -1,9 +0,0 @@
-(defproject lupakartta/messenger "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-
-  :dependencies [[com.novemberain/langohr "5.0.0"]  ; An idiomatic Clojure client for RabbitMQ that embraces the AMQP 0.9.1 model
-                 [org.clojure/clojure "1.8.0"]
-                 ])
